504e7806a034b3f58719531cb0fa640c11b37e98
mohsenis <mohseni.s.alireza@gmail.com>
2015-07-06 17:03:29 -0700

    small debug

--- TNAtoolAPI-Webapp/WebContent/js/onMapReport.js
+++ TNAtoolAPI-Webapp/WebContent/js/onMapReport.js
@@ -177,7 +177,7 @@ function showOnMapReport(lat, lon, date, x){
 			var html2 = '<tfoot>'+tmp+'</tfoot>';
 
 			var popupOptions = {'offset': L.point(0, -8)};
-			$.each(data.MapTr.MapAgencies, function(i,item){
+			$.each(data.MapTR.MapAL, function(i,item){
 				html += '<td>'+item.Name+'</td>'+
 						'<td>'+numberWithCommas(item.RoutesCount)+'</td>'+
 						'<td>'+numberWithCommas(item.MapSL.length)+'</td>'+
@@ -270,7 +270,7 @@ function showOnMapReport(lat, lon, date, x){
 			html += '<thead>'+tmp+'</thead><tbody>';
 
 			var popupOptions = {'offset': L.point(0, -8)};
-			$.each(data.MapG.MapCounties, function(i,item){
+			$.each(data.MapG.MapCL, function(i,item){
 				html += '<td>'+item.Name.replace(' County','')+'</td>'+
 						'<td>'+numberWithCommas(item.MapTL.length)+'</td>'+
 						'<td>'+numberWithCommas(item.MapBL.length)+'</td>'+
@@ -291,14 +291,14 @@ function showOnMapReport(lat, lon, date, x){
 					spiderfyOnMaxZoom: true, showCoverageOnHover: false, zoomToBoundsOnClick: true, singleMarkerMode: true
 				});
 
-				$.each(item.MapBlocks, function(j,jtem){						
+				$.each(item.MapBL, function(j,jtem){						
 						var marker = L.marker([jtem.Lat,jtem.Lng]/*, {icon: onMapIcon}*/);						
 						marker.bindPopup('<b>Block ID:</b> '+jtem.ID+'<br><b>Type:</b> '+jtem.Type+'<br><b>Population:</b> '+numberWithCommas(jtem.Population)+'<br><b>County:</b> '+jtem.County+'<br><b>Land Area:</b> '+ numberWithCommas(Math.round(parseFloat(jtem.LandArea)*0.0000386102)/100)+' mi<sup>2</sup>',popupOptions);
 						tmpBlockCluster.addLayer(marker);	
 				});
 
 				blockCluster.push(tmpBlockCluster);				
-				$.each(item.MapTracts, function(k,ktem){
+				$.each(item.MapTL, function(k,ktem){
 					var tractmarker = L.marker([ktem.Lat,ktem.Lng]/*, {icon: onMapIcon}*/);
 					tractmarker.bindPopup('<b>Tract ID:</b> '+ktem.ID+'<br><b>Population:</b> '+numberWithCommas(ktem.Population)+'<br><b>County:</b> '+ktem.County+'<br><b>Land Area:</b> '+ numberWithCommas(Math.round(parseFloat(ktem.LandArea)*0.0000386102)/100)+' mi<sup>2</sup>',popupOptions);
 					tmpTractCluster.addLayer(tractmarker);
