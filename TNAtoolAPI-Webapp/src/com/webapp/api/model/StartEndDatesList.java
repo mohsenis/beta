d46921174899e34f71d474579168159d4a861763
mohsenis <mohseni.s.alireza@gmail.com>
2015-08-25 17:53:04 -0700

    Add class for a list of start and end dates (used for tooltips)

---
+++ TNAtoolAPI-Webapp/src/com/webapp/api/model/StartEndDatesList.java
@@ -0,0 +1,18 @@
+package com.webapp.api.model;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+@XmlRootElement(name = "StartEndDatesList")
+public class StartEndDatesList {
+	
+	@XmlElement(name = "SEDList")
+	public Collection<StartEndDates> SEDList = new ArrayList<StartEndDates>();
+
+}
